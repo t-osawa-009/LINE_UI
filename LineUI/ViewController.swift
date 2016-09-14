b76ef2d01cb643a1308241c2915d02c999660910
takuya <takuya@takuya-no-macbook.local>
2016-09-15 07:40:46 +0900

    Initial Commit

---
+++ LineUI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LineUI
+//
+//  Created by takuya on 2016/09/15.
+//  Copyright © 2016年 takuya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
