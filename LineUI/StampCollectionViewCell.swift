a0a25b3310cd3d7c715099fc0e65a24df769df19
takuya <takuya@takuya-no-MacBook.local>
2016-09-15 08:14:02 +0900

    add cell

---
+++ LineUI/StampCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  StampCollectionViewCell.swift
+//  LineUI
+//
+//  Created by takuya on 2016/09/15.
+//  Copyright © 2016年 takuya. All rights reserved.
+//
+
+import UIKit
+
+final class StampCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+
+    }
+
+}
